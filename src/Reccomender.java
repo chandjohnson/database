20edcfa8e29b23bec183d5008a62ab5f9e8f9aa6
Chandler Johnson <surfacetowhere@gmail.com>
2017-04-05 13:06:26 -0400

    First commit

---
+++ src/Reccomender.java
@@ -0,0 +1,81 @@
+import java.sql.*;
+
+public class Reccomender
+{
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost:3306/movie_reccomender?useSSL=false"; //YOUR DATABASE NAME
+
+	   static final String USER = "root";
+	   static final String PASS = "password"; //SET AS YOUR PASSWORD
+	   
+	   Statement statement = null;
+	   Connection connection = null;
+	
+	public static void main(String[] args)
+	{
+		Reccomender reccomender = new Reccomender();
+		reccomender.initDB();
+	}
+	
+	private void initDB()
+	{	
+		try{
+		      //Registering the JDBC DRIVER
+		      Class.forName(JDBC_DRIVER);
+
+		      //Opening the connection to the database
+		      System.out.print("Connecting to database...");
+		      connection = DriverManager.getConnection(DB_URL, USER, PASS);
+		      System.out.println("[OK]");
+
+		      //Initializing the database
+		      System.out.print("Creating database...");
+		      statement = connection.createStatement();
+		      System.out.println("[OK]");
+		} catch (Exception e)
+		{
+			System.out.println("[FAIL] " + e.toString());
+		}
+		
+		//Create each table here
+		createMoviesTable();
+	}
+	
+	private void createMoviesTable()
+	{
+		try {
+			String sqlCreateMovies = "CREATE TABLE movies ("
+	    		+"id							int	not null,"
+	    		+"title							varchar(255),"
+	    		+"imdbID						int,"
+	    		+"spanishTitle					varchar(255),"
+	    		+"imdbPictureURL				varchar(255),"
+	    		+"year							int,"
+	    		+"rtID							varchar(255),"
+	    		+"rtAllCriticsRating			double,"
+	    		+"rtAllCriticsNumReviews		int,"
+	    		+"rtAllCriticsNumFresh			int,"
+	    		+"rtAllCriticsNumRotten			int,"
+	    		+"rtAllCriticsScore				int,"
+	    		+"rtTopCriticsRating			double,"
+	    		+"rtTopCriticsNumReviews		int,"
+	    		+"rtTopCriticsNumFresh			int,"
+	    		+"rtTopCriticsNumRotten			int,"
+	    		+"rtTopCriticsScore				int,"
+	    		+"rtAudienceRating				double,"
+	    		+"rtAudienceNumRatings			int,"
+	    		+"rtAudienceScore				double,"
+	    		+"rtPictureURL 					varchar(255),"
+	    		+"primary key (id));";
+			
+			System.out.print("Creating table movies...");
+		    statement.executeUpdate(sqlCreateMovies);
+		    System.out.println("[OK]");
+	      
+	      System.out.println("Worked");
+		} catch (Exception e)
+		{
+			System.out.println("[WARN] " + e.toString());
+		}
+	}
+}
