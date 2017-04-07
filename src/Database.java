5852713793742f099607b18b832ecd0b85df38a7
Chandler Johnson <surfacetowhere@gmail.com>
2017-04-07 11:57:18 -0400

    Added many more tables

--- src/Database.java
+++ src/Database.java
@@ -8,7 +8,7 @@ public class Database
 	static final String DRIVER = "com.mysql.jdbc.Driver";  
 	static final String LOCATION = "jdbc:mysql://localhost:3306/movie_reccomender?useSSL=false"; //YOUR DATABASE NAME
 	static final String USERNAME = "root";
-	static final String PASSWORD = "password"; //SET AS YOUR PASSWORD
+	static final String PASSWORD = "windowlicker"; //SET AS YOUR PASSWORD
 	   
 	Statement statement = null;
 	Connection connection = null;
@@ -16,28 +16,33 @@ public class Database
 	public Database()
 	{	
 		try{
-		      //Registering the JDBC DRIVER
-		      Class.forName(DRIVER);
-
-		      //Opening the connection to the database
-		      System.out.print("Connecting to database...");
-		      connection = DriverManager.getConnection(LOCATION, USERNAME, PASSWORD);
-		      System.out.println("[OK]");
-
-		      //Initializing the database
-		      System.out.print("Creating database...");
-		      statement = connection.createStatement();
-		      System.out.println("[OK]");
+			//Registering the JDBC DRIVER
+			Class.forName(DRIVER);
+			
+			//Opening the connection to the database
+			System.out.print("Connecting to database...");
+			connection = DriverManager.getConnection(LOCATION, USERNAME, PASSWORD);
+			System.out.println("[OK]");
+			
+			//Initializing the database
+			System.out.print("Creating database...");
+			statement = connection.createStatement();
+			System.out.println("[OK]");
+			
+			//Create each table here
+			createMoviesTable();
+			createGenresTable();
+			createDirectorsTable();
+			createActorsTable();
+			createCountriesTable();
+			createLocationsTable();
+			createTagsTable();
+			createMovieTagsTable();
+			
 		} catch (Exception e)
 		{
 			System.out.println("[FAIL] " + e.toString());
 		}
-		
-		//Create each table here
-		createMoviesTable();
-		createGenresTable();
-		createDirectorsTable();
-		createActorsTable();
 	}
 	
 	private void loadData()
@@ -87,9 +92,9 @@ public class Database
 	{
 		try {
 			String sqlCreateGenres = "CREATE TABLE movie_genres ("
-    		  		+"movieID			int	not null,"
-    		  		+"genre 			varchar(255),"
-    		  		+"primary key (movieID, genre));";
+    		  	+"movieID			int	not null,"
+    		  	+"genre 			varchar(255),"
+    		  	+"primary key (movieID, genre));";
 			
 			System.out.print("Creating table genres...");
 		    statement.executeUpdate(sqlCreateGenres);
@@ -105,10 +110,10 @@ public class Database
 	{
 		try {
 			String sqlCreateDirectors = "CREATE TABLE movie_directors ("
-    		  		+"movieID				int	not null,"
-    		  		+"directorID			varchar(255),"
-    		  		+"directorName			varchar(255),"
-    		  		+"primary key (directorID, movieID));";
+    		  	+"movieID				int	not null,"
+    		  	+"directorID			varchar(255),"
+    		  	+"directorName			varchar(255),"
+    		  	+"primary key (directorID, movieID));";
 			
 			System.out.print("Creating table diretors...");
 		    statement.executeUpdate(sqlCreateDirectors);
@@ -123,15 +128,111 @@ public class Database
 	private void createActorsTable()
 	{
 		try {
-			String sqlCreateActors = "create table movie_actors ("
-    		  		+"movieID						int not null,"
-    		  		+"actorID 						varchar(255),"
-    		  		+"actorName						varchar(255),"
-    		  		+"ranking						int,"
-    		  		+"primary key (actorID, movieID));";
+			String sqlCreateActors = "CREATE TABLE movie_actors ("
+    		  	+"movieID						int not null,"
+    		  	+"actorID 						varchar(255),"
+    		  	+"actorName						varchar(255),"
+    		  	+"ranking						int,"
+    		  	+"primary key (actorID, movieID));";
 			
 			System.out.print("Creating table actors...");
 		    statement.executeUpdate(sqlCreateActors);
+		    System.out.println("[OK]");
+		    
+		} catch (Exception e)
+		{
+			System.out.println("[WARN] " + e.toString());
+		}
+	}
+	
+	private void createCountriesTable()
+	{
+		try {
+			String sqlCreateCountries = "CREATE TABLE movie_countries ("
+    			+"movieID					int	not null,"
+    		  	+"country 					varchar(255),"
+    		  	+"primary key (movieID));";
+			
+			System.out.print("Creating table countries...");
+		    statement.executeUpdate(sqlCreateCountries);
+		    System.out.println("[OK]");
+		    
+		} catch (Exception e)
+		{
+			System.out.println("[WARN] " + e.toString());
+		}
+	}
+	
+	private void createLocationsTable()
+	{
+		try {
+			String sqlCreateLocations = "CREATE TABLE movie_locations ("
+    		  	+"movieID						int	not null,"
+    		  	+"location1 					varchar(255),"
+    		  	+"location2						varchar(255),"
+    		  	+"location3						varchar(255),"
+    		  	+"location4						varchar(255),"
+    		  	+"primary key (movieID, location1, location2, location3, location4));";
+			
+			System.out.print("Creating table locations...");
+		    statement.executeUpdate(sqlCreateLocations);
+		    System.out.println("[OK]");
+		    
+		} catch (Exception e)
+		{
+			System.out.println("[WARN] " + e.toString());
+		}
+	}
+	
+	private void createTagsTable()
+	{
+		try {
+			String sqlCreateTags = "CREATE TABLE tags ("
+    		  		+"id					int	not null,"
+    		  		+"value 				varchar(255),"
+    		  		+"primary key (id));";
+			
+			System.out.print("Creating table tags...");
+		    statement.executeUpdate(sqlCreateTags);
+		    System.out.println("[OK]");
+		    
+		} catch (Exception e)
+		{
+			System.out.println("[WARN] " + e.toString());
+		}
+	}
+	
+	private void createMovieTagsTable()
+	{
+		try {
+			String sqlCreateMovieTags = "create table movie_tags ("
+    		  		+"movieID				int	not null,"
+    		  		+"tagID 				int,"
+    		  		+"tagWeight				int,"
+    		  		+"primary key (movieID, tagID));";
+			
+			System.out.print("Creating table movie tags...");
+		    statement.executeUpdate(sqlCreateMovieTags);
+		    System.out.println("[OK]");
+		    
+		} catch (Exception e)
+		{
+			System.out.println("[WARN] " + e.toString());
+		}
+	}
+	
+	private void createUserTaggedTable()
+	{
+		try {
+			String sqlCreateTaggedTable = "create table user_taggedmovies_timestamps ("
+    		  		+"userID					int	not null,"
+    		  		+"movieID   				int,"
+    		  		+"tagID  					int,"
+    		  		+"timestamp					long,"
+    		  		+"primary key (userID, movieID, tagID));";
+			
+			System.out.print("Creating table user tagged...");
+		    statement.executeUpdate(sqlCreateTaggedTable);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
