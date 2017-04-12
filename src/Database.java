460c6a0a6be421153d7bbc9fc574cdfc9de28dd1
Chandler Johnson <surfacetowhere@gmail.com>
2017-04-12 15:13:29 -0400

    Added data loading from most data files

--- src/Database.java
+++ src/Database.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Database
@@ -15,7 +12,7 @@ public class Database
 	static final String USERNAME = "root";
 	static final String PASSWORD = "windowlicker"; //SET AS YOUR PASSWORD
 	   
-	Statement statement = null;
+	Statement database = null;
 	Connection connection = null;
 	   
 	public Database()
@@ -30,25 +27,9 @@ public class Database
 			System.out.println("[OK]");
 			
 			//Initializing the database
-			System.out.print("Creating database...");
-			statement = connection.createStatement();
+			System.out.print("Initalizing...");
+			database = connection.createStatement();
 			System.out.println("[OK]");
-			
-			//Create each table here
-			createMoviesTable();
-			createGenresTable();
-			createDirectorsTable();
-			createActorsTable();
-			createCountriesTable();
-			createLocationsTable();
-			createTagsTable();
-			createMovieTagsTable();
-			createUserTaggedTable();
-			createTaggedMoviesTable();
-			createRatedMoviesTable();
-			
-			//Populate each table here
-			loadMovies();
 			
 		} catch (Exception e)
 		{
@@ -56,34 +37,72 @@ public class Database
 		}
 	}
 	
-	private void loadMovies()
+	public void createTables()
 	{
-		//Load the data from the .dat files here
-
-		System.out.print("Pupulating movies table (this may take some time)...");
+		createMoviesTable();
+		createGenresTable();
+		createDirectorsTable();
+		createActorsTable();
+		createCountriesTable();
+		createLocationsTable();
+		createTagsTable();
+		createMovieTagsTable();
+		createUserTaggedTable();
+		createTaggedMoviesTable();
+		createRatedMoviesTable();
+	}
+	
+	public void populateTables()
+	{
+		//Need to fix a few errors on the commented tables
+		
+		loadData("movies");
+		loadData("movie_genres");
+		loadData("movie_directors");
+		loadData("movie_actors");
+		//loadData("movie_countries");
+		//loadData("movie_locations");
+		loadData("movie_tags");
+		//loadData("user_taggedmovies_timestamps");
+		//loadData("user_taggedmovies");
+		loadData("user_ratedmovies");
+	}
+	
+	private void loadData(String table)
+	{
+		System.out.print("Populating " + table + " table");
 		try
 		{
 			BufferedReader br;
-			br = new BufferedReader(new FileReader("Rotten Tomatos Dataset/movies.dat"));
+			br = new BufferedReader(new FileReader("Rotten Tomatos Dataset/" + table + ".dat"));
 			String line;
 			
-			String sqlEmptyTable = "DELETE FROM movies;";
-			statement.executeUpdate(sqlEmptyTable);
+			String sqlEmptyTable = "DELETE FROM " + table + ";";
+			database.executeUpdate(sqlEmptyTable);
+			
+			int loadBar = 0;
 			
 			br.readLine();
 			
 			while ((line = br.readLine()) != null)
 			{
-				String [] dataChunk = line.replace("\'", "\\'").replace("\"", "\\\"").split("\t");
-				if (dataChunk.length == 21)
+				String [] dataChunk = line.replace("\'", "\\'").split("\t");
+				StringBuilder sqlInsert = new StringBuilder();
+				
+				sqlInsert.append("INSERT INTO " + table + " VALUES (");
+				
+				for(int i = 0; i < dataChunk.length; ++i)
 				{
-					String sqlInsert = "INSERT INTO movies VALUES (" + dataChunk[0] +", \'" + dataChunk[1] + "\', " + dataChunk[2] + ", \'" + dataChunk[3] + "\', \'" + dataChunk[4] + "\', " + dataChunk[5] + ", \'" + dataChunk[6] + "\', " + dataChunk[7] + ", " + dataChunk[8] + ", " + dataChunk[9] + ", " + dataChunk[10] + ", " + dataChunk[11] + ", " + dataChunk[12] + ", " + dataChunk[13] + ", " + dataChunk[14] + ", " + dataChunk[15] + ", " + dataChunk[16] + ", " + dataChunk[17] + ", " + dataChunk[18] + ", " + dataChunk[19] + ", \'" + dataChunk[20] + "\');";
-					statement.executeUpdate(sqlInsert);
+					// Special case
+					if(dataChunk[i].equals("\\N")) dataChunk[i] = "0";
+					
+					sqlInsert.append("'" + dataChunk[i] + "'");
+					
+					if(i == dataChunk.length - 1) sqlInsert.append(");");
+					else sqlInsert.append(",");
 				}
-				else
-				{
-					System.out.println("Omitted unformatted tuple " + line);
-				}
+				database.executeUpdate(sqlInsert.toString());
+				if(loadBar++ % 4000 == 0) System.out.print(".");
 			}
 			br.close();
 			System.out.println("[OK]");
@@ -96,6 +115,9 @@ public class Database
 	private void createMoviesTable()
 	{
 		try {
+			String sqlDropMovies = "DROP TABLE IF EXISTS movies;";
+			database.executeUpdate(sqlDropMovies);
+			
 			String sqlCreateMovies = "CREATE TABLE movies ("
 	    		+"id							int	not null,"
 	    		+"title							varchar(255),"
@@ -121,55 +143,64 @@ public class Database
 	    		+"primary key (id));";
 			
 			System.out.print("Creating table movies...");
-		    statement.executeUpdate(sqlCreateMovies);
+		    database.executeUpdate(sqlCreateMovies);
 		    System.out.println("[OK]");
 	      
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createGenresTable()
 	{
 		try {
+			String sqlDropGenres = "DROP TABLE IF EXISTS movie_genres;";
+			database.executeUpdate(sqlDropGenres);
+			
 			String sqlCreateGenres = "CREATE TABLE movie_genres ("
     		  	+"movieID			int	not null,"
     		  	+"genre 			varchar(255),"
     		  	+"primary key (movieID, genre));";
 			
-			System.out.print("Creating table genres...");
-		    statement.executeUpdate(sqlCreateGenres);
+			System.out.print("Creating table movie_genres...");
+		    database.executeUpdate(sqlCreateGenres);
 		    System.out.println("[OK]");
 	      
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createDirectorsTable()
 	{
 		try {
+			String sqlDropDirectors = "DROP TABLE IF EXISTS movie_directors;";
+			database.executeUpdate(sqlDropDirectors);
+			
 			String sqlCreateDirectors = "CREATE TABLE movie_directors ("
     		  	+"movieID				int	not null,"
     		  	+"directorID			varchar(255),"
     		  	+"directorName			varchar(255),"
     		  	+"primary key (directorID, movieID));";
 			
-			System.out.print("Creating table diretors...");
-		    statement.executeUpdate(sqlCreateDirectors);
+			System.out.print("Creating table movie_directors...");
+		    database.executeUpdate(sqlCreateDirectors);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createActorsTable()
 	{
 		try {
+			String sqlDropActors = "DROP TABLE IF EXISTS movie_actors;";
+			database.executeUpdate(sqlDropActors);
+			
 			String sqlCreateActors = "CREATE TABLE movie_actors ("
     		  	+"movieID						int not null,"
     		  	+"actorID 						varchar(255),"
@@ -177,37 +208,43 @@ public class Database
     		  	+"ranking						int,"
     		  	+"primary key (actorID, movieID));";
 			
-			System.out.print("Creating table actors...");
-		    statement.executeUpdate(sqlCreateActors);
+			System.out.print("Creating table movie_actors...");
+		    database.executeUpdate(sqlCreateActors);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createCountriesTable()
 	{
 		try {
+			String sqlDropCountries = "DROP TABLE IF EXISTS movie_countries;";
+			database.executeUpdate(sqlDropCountries);
+			
 			String sqlCreateCountries = "CREATE TABLE movie_countries ("
     			+"movieID					int	not null,"
     		  	+"country 					varchar(255),"
     		  	+"primary key (movieID));";
 			
-			System.out.print("Creating table countries...");
-		    statement.executeUpdate(sqlCreateCountries);
+			System.out.print("Creating table movie_countries...");
+		    database.executeUpdate(sqlCreateCountries);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createLocationsTable()
 	{
 		try {
+			String sqlDropLocations = "DROP TABLE IF EXISTS movie_locations;";
+			database.executeUpdate(sqlDropLocations);
+			
 			String sqlCreateLocations = "CREATE TABLE movie_locations ("
     		  	+"movieID						int	not null,"
     		  	+"location1 					varchar(255),"
@@ -216,56 +253,65 @@ public class Database
     		  	+"location4						varchar(255),"
     		  	+"primary key (movieID, location1, location2, location3, location4));";
 			
-			System.out.print("Creating table locations...");
-		    statement.executeUpdate(sqlCreateLocations);
+			System.out.print("Creating table movie_locations...");
+		    database.executeUpdate(sqlCreateLocations);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createTagsTable()
 	{
 		try {
+			String sqlDropTags = "DROP TABLE IF EXISTS tags;";
+			database.executeUpdate(sqlDropTags);
+			
 			String sqlCreateTags = "CREATE TABLE tags ("
     		  		+"id					int	not null,"
     		  		+"value 				varchar(255),"
     		  		+"primary key (id));";
 			
 			System.out.print("Creating table tags...");
-		    statement.executeUpdate(sqlCreateTags);
+		    database.executeUpdate(sqlCreateTags);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createMovieTagsTable()
 	{
 		try {
+			String sqlDropMovieTags = "DROP TABLE IF EXISTS movie_tags;";
+			database.executeUpdate(sqlDropMovieTags);
+			
 			String sqlCreateMovieTags = "CREATE TABLE movie_tags ("
     		  		+"movieID				int	not null,"
     		  		+"tagID 				int,"
     		  		+"tagWeight				int,"
     		  		+"primary key (movieID, tagID));";
 			
-			System.out.print("Creating table movie tags...");
-		    statement.executeUpdate(sqlCreateMovieTags);
+			System.out.print("Creating table movie_tags...");
+		    database.executeUpdate(sqlCreateMovieTags);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createUserTaggedTable()
 	{
 		try {
+			String sqlDropUserTagged = "DROP TABLE IF EXISTS user_taggedmovies_timestamps;";
+			database.executeUpdate(sqlDropUserTagged);
+			
 			String sqlCreateTaggedTable = "CREATE TABLE user_taggedmovies_timestamps ("
     		  		+"userID					int	not null,"
     		  		+"movieID   				int,"
@@ -273,19 +319,22 @@ public class Database
     		  		+"timestamp					long,"
     		  		+"primary key (userID, movieID, tagID));";
 			
-			System.out.print("Creating table user tagged...");
-		    statement.executeUpdate(sqlCreateTaggedTable);
+			System.out.print("Creating table user_taggedmovies_timestamps...");
+		    database.executeUpdate(sqlCreateTaggedTable);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createTaggedMoviesTable()
 	{
 		try {
+			String sqlDropUserTaggedMovies = "DROP TABLE IF EXISTS user_taggedmovies;";
+			database.executeUpdate(sqlDropUserTaggedMovies);
+			
 			String sqlTaggedMoviesTable = "CREATE TABLE user_taggedmovies ("
     		  		+"userID					int	not null,"
     		  		+"movieID					int,"
@@ -298,19 +347,22 @@ public class Database
     		  		+"date_second				int,"
     		  		+"primary key (userID, movieID, tagID));";
 			
-			System.out.print("Creating table tagged movies...");
-		    statement.executeUpdate(sqlTaggedMoviesTable);
+			System.out.print("Creating table user_taggedmovies...");
+		    database.executeUpdate(sqlTaggedMoviesTable);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 	
 	private void createRatedMoviesTable()
 	{
 		try {
+			String sqlDropRatedMovies = "DROP TABLE IF EXISTS user_ratedmovies;";
+			database.executeUpdate(sqlDropRatedMovies);
+			
 			String sqlRatedMovies = "CREATE TABLE user_ratedmovies ("
 	  		  		+"userID					int	not null,"
 	  		  		+"movieID					int,"
@@ -323,13 +375,13 @@ public class Database
 	  		  		+"date_second				int,"
 	  		  		+"primary key (userID, movieID));";
 			
-			System.out.print("Creating table rated movies...");
-		    statement.executeUpdate(sqlRatedMovies);
+			System.out.print("Creating table user_ratedmovies movies...");
+		    database.executeUpdate(sqlRatedMovies);
 		    System.out.println("[OK]");
 		    
 		} catch (Exception e)
 		{
-			System.out.println("[WARN] " + e.toString());
+			System.out.println("[FAIL] " + e.toString());
 		}
 	}
 
--- src/Recommender.java
+++ src/Recommender.java
@@ -3,5 +3,7 @@ public class Recommender
 	public static void main(String[] args)
 	{
 		Database db = new Database();
+		db.createTables();
+		db.populateTables();
 	}
 }
